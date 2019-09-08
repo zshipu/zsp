a00175cd027aaf019bcd8036c920aaec1ee63d00
edwin <liliang8858@126.com>
2019-09-08 19:13:50 +0800

    update

--- src/store/main.go
+++ src/store/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"gowww/store/models"
 	"gowww/store/storage"
@@ -10,13 +11,14 @@ func main() {
 	fmt.Println("hello")
 	db := storage.NewLevDb()
 	article := models.NewArticle()
-	article.Content = "第一个文章3"
+	article.Content = "第一个文章5"
 	article.Type = "t"
-	article.Title = "第一个文章3"
+	article.Title = "第一个文章5"
 	db.CreateItem(article)
 
 	articles, _ := db.GetAll()
 
-	fmt.Printf("%v", articles)
+	bytes, _ := json.MarshalIndent(articles, "", "")
+	fmt.Printf("%s", string(bytes))
 
 }
