0eb503cf1c673cbf0bbd6ec604000d588e7e244c
edwin <liliang8858@126.com>
2019-10-23 21:16:27 +0800

    update

--- src/tongji/res.js
+++
@@ -1,5 +0,0 @@
-try{
-    BusuanziCallback_619841523380(
-        {"site_uv":10,"page_pv":1,"version":2.4,"site_pv":407}
-     );
-}catch(e){}
--- src/tongji/tongji.go
+++ src/tongji/tongji.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 	"github.com/seefan/gossdb"
@@ -8,6 +10,16 @@ import (
 	"log"
 )
 
+type ReqUrlParam struct {
+	JsonpCallback  string `params:"jsonpCallback"`
+}
+
+type SiteUvPv struct {
+	SiteUv  int     `json:"site_uv"`
+	PagePv  int     `json:"page_pv"`
+	Version float64 `json:"version"`
+	SitePv  int     `json:"site_pv"`
+}
 func main() {
 	pool, err := gossdb.NewPool(&conf.Config{
 		Host:             "127.0.0.1",
@@ -27,18 +39,60 @@ func main() {
 		return
 	}
 	defer c.Close()
-	c.Set("http://127.0.0.1",`try{BusuanziCallback_619841523380({"site_uv":10,"page_pv":1,"version":2.4,"site_pv":407});}catch(e){}`)
-	re, err := c.Get("http://127.0.0.1")
-	if err != nil {
-		log.Println(err)
-	} else {
-		log.Println(re)
-	}
-
 
 	s := g.Server()
-	s.BindHandler("/busuanzi", func(r *ghttp.Request) {
-		r.Response.Write(`try{BusuanziCallback_619841523380({"site_uv":10,"page_pv":1,"version":2.4,"site_pv":407});}catch(e){}`)
+	s.BindHandler("/pushtsua", func(r *ghttp.Request) {
+		params := new(ReqUrlParam)
+		r.GetToStruct(params)
+		fmt.Println("params"+params.JsonpCallback)
+		fmt.Println("param %v",params)
+
+		refererstr := r.Header.Get("referer")
+		fmt.Println(refererstr)
+
+		strstr, err := c.Get(refererstr)
+		if err != nil {
+			log.Println(err)
+		}
+
+		if len(strstr) > 0 {
+			fmt.Println(strstr)
+
+			var pv SiteUvPv
+			// 将字符串反解析为结构体
+			json.Unmarshal([]byte(strstr), &pv)
+			fmt.Println(pv) //
+
+			pv.PagePv = pv.PagePv+1
+
+			//Marshal失败时err!=nil
+			marstr, err := json.Marshal(pv)
+			if err != nil {
+				fmt.Println("生成json字符串错误")
+			}
+
+			fmt.Println(string(marstr))
+			c.Set(refererstr,string(marstr))
+
+			r.Response.Write(`try{`+params.JsonpCallback+`(`+string(marstr)+`);}catch(e){}`)
+
+		}else{
+			pv :=&SiteUvPv{
+				SiteUv:1,
+				SitePv:1,
+				PagePv:1,
+			}
+
+			//Marshal失败时err!=nil
+			marstr, err := json.Marshal(pv)
+			if err != nil {
+				fmt.Println("生成json字符串错误")
+			}
+			c.Set(refererstr,marstr)
+
+			r.Response.Write(`try{`+params.JsonpCallback+`({"site_uv":1,"page_pv":1,"version":2.4,"site_pv":1});}catch(e){}`)
+		}
+
 	})
 	s.SetPort(5040)
 	s.Run()
